e997ceb94366506282ae91ebc46ed770974157b9
1anKoz <jankozinski@wp.pl>
2024-07-07 14:22:44 +0200

    Create LibraryViewModel to display both shelves and books in one view

--- BookManagerApp/Controllers/LibraryViewModelController.cs
+++ BookManagerApp/Controllers/LibraryViewModelController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookManagerApp.Data;
+using BookManagerApp.Models;
+using BookManagerApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookManagerApp.Controllers
 {
     public class LibraryViewModelController : Controller
     {
+        private readonly LibraryRepository _repository;
+
+        public LibraryViewModelController(LibraryRepository repository)
+        {
+            _repository = repository;
+        }
         public IActionResult Index()
         {
-            return View();
+            LibraryViewModel libraryViewModel = new LibraryViewModel
+            {
+                Shelves = _repository.GetShelves(),
+                Books = _repository.GetBooks(),
+            };
+
+            return View(libraryViewModel);
         }
     }
 }
