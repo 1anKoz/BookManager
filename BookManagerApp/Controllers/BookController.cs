fdb576a07e2b86fe3b0c91be6e93b7da065a0338
1anKoz <jankozinski@wp.pl>
2024-07-07 12:00:26 +0200

    Create BookController and ShelfController and initial views for those two models

---
+++ BookManagerApp/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using BookManagerApp.Data;
+using BookManagerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagerApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Book> books = _context.Books.ToList();
+            return View(books);
+        }
+    }
+}
---
+++ BookManagerApp/Controllers/LibraryViewModelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagerApp.Controllers
+{
+    public class LibraryViewModelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
