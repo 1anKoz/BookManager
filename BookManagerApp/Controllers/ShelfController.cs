242a9cb1afb4103b504bda4c7adc7775246d58c0
1anKoz <jankozinski@wp.pl>
2024-07-13 00:02:41 +0200

    Add shelf create

--- BookManagerApp/Controllers/ShelfController.cs
+++ BookManagerApp/Controllers/ShelfController.cs
@@ -1,7 +1,9 @@
 ﻿using BookManagerApp.Data;
 using BookManagerApp.Interfaces;
 using BookManagerApp.Models;
+using BookManagerApp.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookManagerApp.Controllers
@@ -24,6 +26,17 @@ namespace BookManagerApp.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Shelf shelf)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(shelf);
+            }
+            _shelfRepository.Add(shelf);
+            return RedirectToAction("Index", "Shelf");
         }
     }
 }
