e28fc084516ddea4d814a510b7dac24b10684a36
1anKoz <jankozinski@wp.pl>
2024-07-15 13:51:34 +0200

    Create QuoteRepository IQuoteRepository and QuoteController

---
+++ BookManagerApp/Controllers/QuoteController.cs
@@ -0,0 +1,41 @@
+﻿using BookManagerApp.Interfaces;
+using BookManagerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookManagerApp.Controllers
+{
+    public class QuoteController : Controller
+    {
+        private readonly IQuoteRepository _quoteRepository;
+        private readonly IBookRepository _bookRepository;
+
+        public QuoteController(IQuoteRepository quoteRepository, IBookRepository bookRepository)
+        {
+            _quoteRepository = quoteRepository;
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IActionResult> Create(int? bookId)
+        {
+            var books = await _bookRepository.GetAll();
+            ViewBag.Books = new SelectList(books, "Id", "Title");
+
+            ViewBag.BookId = bookId ?? 0;
+
+            return View();
+        }
+
+        public async Task<IActionResult> Create(Quote quote)
+        {
+            if (!ModelState.IsValid)
+            {
+                var books = await _bookRepository.GetAll();
+                ViewBag.Books = new SelectList(books, "Id", "Name");
+
+                return View(quote);
+            }
+            _quoteRepository.Add(quote);
+            return RedirectToAction("Detail", "Book");
+    }
+}
---
+++ BookManagerApp/Repository/QuoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using BookManagerApp.Data;
+using BookManagerApp.Interfaces;
+using BookManagerApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagerApp.Repository
+{
+    public class QuoteRepository : IQuoteRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuoteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Quote quote)
+        {
+            _context.Add(quote);
+            return Save();
+        }
+
+        public bool Delete(Quote quote)
+        {
+            _context.Remove(quote);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Quote>> GetAllAsync()
+        {
+            return await _context.Quotes.ToListAsync();
+        }
+
+        public async Task<Quote> GetByIdAsync(int id)
+        {
+            return await _context.Quotes.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Quote quote)
+        {
+            _context.Update(quote);
+            return Save();
+        }
+    }
+}
